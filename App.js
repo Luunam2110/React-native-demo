1767142d05400abb232c28416a309f7e343a94df
Lưu văn nam <71889543+Luunam2110@users.noreply.github.com>
2020-12-13 20:54:59 +0700

    update

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ const App: () => React$Node = (props) => {
       <NavigationContainer>
         {
           !props.login?
-            <LognComponent/>
+            <LoginComponent/>
             :
             <Stack.Navigator>
               <Stack.Screen
